9c9ce4987f8cb3b3ee28d7fb6a335ec10da15725
dchu1991 <ren1072.ep99@g2.nctu.edu.tw>
2019-08-24 15:07:16 +0800

    modified:   MC_randomwalk.py

--- MC_randomwalk.py
+++ MC_randomwalk.py
@@ -7,6 +7,8 @@ This is a temporary script file.
 
 import random
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 def rand_walk(n):
     """return the coordinate of x, y after random walks of n steps"""
@@ -32,15 +34,19 @@ if __name__ == '__main__':
     call_msg = '{:.2f} % of walks needs a ride home'
     
     # some parameters for this simulation
-    MCsteps = 10000
-    taxi_call = 4
+    MCsteps = 100000
+    taxi_call = 5
     
-    for i in range(1,31):
+    
+    for i in range(29,31):
         tot_dis = 0
         num_calls = 0
+        conf = np.zeros((2,MCsteps))
         
         for j in range(MCsteps):
             x, y = rand_walk(i)
+            conf[0,j] = x
+            conf[1,j] = y
             d = distance(x, y)
             tot_dis += d
             if d > taxi_call:
@@ -50,3 +56,14 @@ if __name__ == '__main__':
         num_calls /= MCsteps
         print( dis_msg.format(tot_dis, i) )
         print( call_msg.format(num_calls*100))
+        
+        plt.scatter(conf[0], conf[1])
+        plt.grid(True)
+        
+        plt.show()
+        plt.close()
+        
+        plt.hist2d(conf[0], conf[1])
+        plt.grid(True)
+        plt.show()
+        plt.close
